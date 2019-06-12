ffaa484176b60b3a46d28b05f73c672f66cef3ff
Tara Mulvaney <mulvaneytara@gmail.com>
2019-06-12 16:17:37 -0400

    added App.js file, connection to data refused. added rack-cors

--- blog_api/client/src/App.js
+++ blog_api/client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      apiData: null,
+      apiDataLoaded: false,
+    };
+  }
+
+  componentDidMount = async () => {
+    const blogs = await axios.get('http://localhost:3001/blogs');
+    const apiData = blogs.data;
+  }
+
+
+
+  showBlogsOnPage() {
+    return this.state.apiData.map((blogs) => {
+      return (
+        <div className="blog-id" key={blogs.id}>
+          <p className="title">{blogs.title}</p>
+          <span className="content">{blogs.content}</span>
+          <span className="topic">{blogs.topic}</span>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {(this.state.apiDataLoaded) ? this.showBlogsOnPage() : <p>Loading...</p>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- blog_api/db/seeds.rb
+++ blog_api/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+blog_1 = Blog.create(
+  title: "Blog 1",
+  topic: "Stuff",
+  content: "Going on about stuff."
+)
+
+blog_2 = Blog.create(
+  title: "Blog 2",
+  topic: "Things",
+  content: "Going on about things."
+)
+
+blog_3 = Blog.create(
+  title: "Blog 3",
+  topic: "The end",
+  content: "No more blogs."
+)
