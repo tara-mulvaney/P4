08f7795782e3a3d8c86347c2709ebce9573948c4
Tara Mulvaney <mulvaneytara@gmail.com>
2019-06-14 12:31:50 -0400

    some git reversion confusion, this commit should include a working editBlog

--- blog_api/client/src/App.js
+++ blog_api/client/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import {  getAllBlogs } from './service/apiHelper';
 import AddButton from './components/AddButton/AddButton';
 import EditButton from './components/EditButton/EditButton';
-import EditBlog from './components/EditBlog/EditBlog';
 import DeleteButton from './components/DeleteButton/DeleteButton';
 
 class App extends Component {
@@ -36,8 +35,12 @@ class App extends Component {
           <p className="title">{blog.title}</p>
           <span className="content">{blog.content}</span>
         </div>
+<<<<<<< HEAD
         <EditBlog
           blog={blog}/>
+=======
+        <EditButton/>
+>>>>>>> parent of 05eac6f... edit form added, onsubmit i return an error stating can't use redirect outside of a router
         <DeleteButton/>
       </div>
       );
